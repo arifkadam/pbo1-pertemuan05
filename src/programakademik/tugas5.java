799d88677338f85b713bd07dbc76f941bd60c754
arifkadam <62461132+arifkadam@users.noreply.github.com>
2020-05-04 03:10:45 +0700

    BIODATA MAHASISWA UNISKA

--- src/programakademik/tugas5.java
+++ src/programakademik/tugas5.java
@@ -1,4 +1,39 @@
 package programakademik;
-
+import java.util.Scanner; //import scanner untuk imputannya
 public class tugas5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); //object scanner baru
+
+        String nama, nim, jenisKelamin, alamat, nope, pos,  def="Masukan "; //variabel
+
+        System.out.println("|     BIODATA MAHASISWA UNISKA       |");
+        System.out.println("===========================================");
+
+        System.out.print(def + "Nama \t\t: " );
+        nama = input.nextLine(); //memberikan nilai variabel dg menyimpan inputan dari user
+        System.out.print(def + "NIM \t\t: " );
+        nim = input.nextLine();
+        System.out.print(def + "Jenis Kelamin \t: " );
+        jenisKelamin = input.nextLine();
+        System.out.print(def + "Alamat \t\t: " );
+        alamat = input.nextLine();
+        System.out.print(def + "Nomor Handphone\t: " );
+        nope = input.nextLine();
+        System.out.println(def + "program studi\t: ");
+        pos = input.nextLine();
+
+        System.out.println("");
+
+        // Tampilkan datanya
+        System.out.println("+------------------------------------------+");
+        System.out.println("|          Data Biodata Mahasiswa          |");
+        System.out.println("+------------------------------------------+");
+        System.out.println("| Nama\t\t: " + nama);
+        System.out.println("| NIM\t\t: " + nim);
+        System.out.println("| Jenis Kelamin\t: " + jenisKelamin);
+        System.out.println("| Alamat\t: " + alamat);
+        System.out.println("| Nomor HP\t: " + nope);
+        System.out.println("| Program Studi\t: " + pos);
+        System.out.println("+------------------------------------------+");
+    }
 }
